b3a2f022e0d88a4cfbdedd725641097dd5d6c46a
Daniel Rebolledo <danielrebolledo.182@gmail.com>
2022-05-11 00:29:50 -0400

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,95 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <div>
+   <div class="logo">
+     <img src="assets/logo.png" alt="logo">
+   </div>
+   <Avatar 
+    v-for="(personaje, $index) of personajes" 
+    :key="$index" 
+    :nombre="personaje.nombre" 
+    :imagen="personaje.src"
+    />
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Avatar from '@/components/Avatar.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { Avatar },
+  data:() => ({
+    personajes: [
+      {
+        nombre: "Rick Sanchez",
+        src: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+      },
+      {
+        nombre: "Morty Smith",
+        src: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+           
+      },
+      {
+        nombre: "Summer Smith",
+        scr: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+      },
+      {
+        nombre: "Beth Smith",
+        src: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+      },
+      {
+        nombre: "Jerry Smith",
+        src: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+      },
+      {
+        nombre: "Abadango Cluster Princess",
+        src: "https://rickandmortyapi.com/api/character/avatar/6.jpeg",
+      },
+      {
+        nombre: "Abradolf Lincler",
+        src: "https://rickandmortyapi.com/api/character/avatar/7.jpeg",
+      },
+      {
+        nombre: "Adjudicator Rick",
+        src: "https://rickandmortyapi.com/api/character/avatar/8.jpeg",
+      },
+      {
+        nombre: "Agency Director",
+        src: "https://rickandmortyapi.com/api/character/avatar/9.jpeg",
+           
+        },
+        {
+        nombre: "Alan Rails",
+        src: "https://rickandmortyapi.com/api/character/avatar/10.jpeg",
+        },
+        {
+        nombre: "Albert Einstein",
+        src: "https://rickandmortyapi.com/api/character/avatar/11.jpeg",
+        },
+    ],
+  })
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body{
+    padding: 0;
+    margin: 0;
+    width: 100%;
+    background-image: url('../public/assets/background.png');
+    background-size: cover;
+    background-attachment: fixed;
+    background-position: center;
+    
+  }
+  .logo{
+    text-align: center;
+    width: 100%;
+  }
+  .logo img{
+    width: 100%;
+    max-width: 800px;
+  }
+
+  
 </style>
---
+++ src/components/Avatar.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+   <div class="contenedor">
+      <img 
+      :src="imagen" 
+      alt="" 
+      class="avatarImg">
+      <p class="nombre"> {{nombre}}</p>
+    </div>
+ </div>
+</template>
+<script>
+
+export default {
+  props: ["nombre",  "imagen"]
+  
+}
+</script>
+<style>
+.contenedor{
+    text-align: center; 
+    margin-bottom: 100px;
+  }
+  .nombre{
+    color: green; 
+    font-size: 40px; 
+    margin: 0px;
+  }
+  .avatarImg{
+      border-radius: 50%; 
+      height:200px;
+  }
+</style>
